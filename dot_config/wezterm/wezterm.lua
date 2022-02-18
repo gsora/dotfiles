249255dba9ba2194b6dd38ce16c95f9d7b178755
Gianguido Sorà <me@gsora.xyz>
2022-02-18 11:13:07 +0100

    move to iosevka

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require 'wezterm';
 
 return {
 	font = wezterm.font_with_fallback({
-		"SF Mono",
+		"Iosevka SS11",
 		"BlexMono Nerd Font"
 	}),
 
-	font_size = 14,
+	font_size = 16,
 	color_scheme = "ayu",
 	freetype_load_target = "Light",
 }

084983c5c8c9d0e27ff7bb00815bc38f0ae5d933
Gianguido Sorà <gsora@users.noreply.github.com>
2024-12-02 11:05:16 +0100

    *: by-os config for git and fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,4 +4,18 @@ end
 set -gx EDITOR hx
 set -gx CHARON_REPO $HOME/Documents/Work/charon
 set -gx PATH $HOME/Code/renode_1.15.2-dotnet_portable $HOME/go/bin $HOME/.cargo/bin $PATH
-fish_add_path -U /home/gsora/.local/bin
+
+set -gx GIT_CONFIG_COUNT 1
+set -gx GIT_CONFIG_KEY_0 "home.dir"
+set -gx GIT_CONFIG_VALUE_0 "$HOME"
+
+switch (uname)
+    case Darwin
+        # do things for macOS
+        fish_add_path -U /opt/homebrew/bin
+    case Linux
+        # do things for Linux
+        fish_add_path -U /home/gsora/.local/bin
+    case '*'
+        # do things for other OSs
+end

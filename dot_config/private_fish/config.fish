5d935e26654cef2d3631f791f7f91c6b81a5a35f
Gianguido Sorà <gsora@users.noreply.github.com>
2024-12-02 11:05:42 +0100

    fish: don't set GIT_CONFIG vars

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,10 +5,6 @@ set -gx EDITOR hx
 set -gx CHARON_REPO $HOME/Documents/Work/charon
 set -gx PATH $HOME/Code/renode_1.15.2-dotnet_portable $HOME/go/bin $HOME/.cargo/bin $PATH
 
-set -gx GIT_CONFIG_COUNT 1
-set -gx GIT_CONFIG_KEY_0 "home.dir"
-set -gx GIT_CONFIG_VALUE_0 "$HOME"
-
 switch (uname)
     case Darwin
         # do things for macOS
